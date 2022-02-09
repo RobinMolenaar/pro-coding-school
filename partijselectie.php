af09e9097922338affc5184d289bf4c29324414a
britney257205 <78146127+britney257205@users.noreply.github.com>
2022-02-09 15:02:07 +0100

    Update partijselectie.php

--- partijselectie.php
+++ partijselectie.php
@@ -4,9 +4,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="mystyle.css">
+    <title>Partij Selectie</title>
 </head>
 <body>
-    
+    <header class="heading-container">
+        <p class="heading-text">Kies hier u gewenste partij.</p>
+    </header>
+    <br>
+    <!-- partijen -->
+    <table>
+        <tr>
+            <th><img class="img"src="img/placeholder.jpg" alt="Placeholder"></th>
+            <th>
+                <h3>Partij</h3>
+                <p>De informatie over de partij.</P>
+            </th>
+        </tr>
+        <tr>
+            <th><img class="img"src="img/placeholder.jpg" alt="Placeholder"></th>
+            <th>
+                <h3>Partij</h3>
+                <p>De informatie over de partij.</P>
+            </th>
+        </tr>
+        <tr>
+            <th><img class="img"src="img/placeholder.jpg" alt="Placeholder"></th>
+            <th>
+                <h3>Partij</h3>
+                <p>De informatie over de partij.</P>
+            </th>
+        </tr>
+        <tr>
+            <th><img class="img"src="img/placeholder.jpg" alt="Placeholder"></th>
+            <th>
+                <h3>Partij</h3>
+                <p>De informatie over de partij.</P>
+            </th>
+        </tr>
+        <tr>
+            <th><img class="img"src="img/placeholder.jpg" alt="Placeholder"></th>
+            <th>
+                <h3>Partij</h3>
+                <p>De informatie over de partij.</P>
+            </th>
+        </tr>
+    </table>
+    <br>
+    <button class='welcome-button' type='button'>Volgende</button> 
 </body>
 </html>
