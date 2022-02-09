7785b66daec6d281cd5e91e0a48aa5faf36097bc
Robin <robinmolenaar662@gmail.com>
2022-02-09 13:30:05 +0100

    commit

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="mystyle.css">
 </head>
 <body>
-    
+    <header class="heading-container">
+        <p class="heading-text">Welkom bij het stem bureau</p>   
+        <button class='welcome-button' type='button'> Breng uw stem uit</button>     
+    </header>
+
 </body>
 </html>
