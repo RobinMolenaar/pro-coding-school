d706df29d82aac433ed8d2d8b0f478eeecf023d5
Robin <robinmolenaar662@gmail.com>
2022-02-09 14:30:52 +0100

    commit

--- index.php
+++ index.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Welkom</title>
     <link rel="stylesheet" href="mystyle.css">
 </head>
-<body>
-    <header class="heading-container">
-        <p class="heading-text">Welkom bij het stem bureau</p>   
-        <button class='welcome-button' type='button'> Breng uw stem uit</button>     
-    </header>
+<body class="welcome-body">
+    <div class="heading-container">
+        <div class='heading-text-container'>
+            <p class="heading-text">Welkom bij het stem bureau</p>   
+            <input type="text">
+            <button onclick="location.href='partijselectie.php'" class='welcome-button' type='button'></button>
+        </div>     
+    </div>   
 
 </body>
 </html>
--- inlog.php
+++ inlog.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="mystyle.css">
 </head>
 <body>
-    
+    <div class="heading-container">
+            <div class='heading-text-container'>
+                <p class="heading-text">Vul hier uw code in</p>   
+                <button onclick="location.href='inlog.php'" class='welcome-button' type='button'> Breng uw stem uit</button>
+            </div>     
+        </div>   
 </body>
 </html>
