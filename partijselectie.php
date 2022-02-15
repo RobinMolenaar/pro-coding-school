c824f82bf960f94aa95c9cea59f7e0277ba42ae3
xjocelyx <jocelyjoy@outlook.com>
2022-02-15 22:46:33 +0100

    mergupdate

--- partijselectie.php
+++ partijselectie.php
@@ -8,20 +8,20 @@ $result = $con->query($sql);
 
 <body>
   <div class="container">
-<?php
-if($result->num_rows > 0){
-  while($row = $result->fetch_assoc()){
-      echo "<div class='Partijvak'>";
-      echo "<img src='".$row['IMG']."'class='Partijlogo'>";
-      echo "<div class='Partijoprij'>";
-    echo "<a href='persoonselectie.php?Partij=".$row['ID']."' class='Partijknop'>".$row['Name']."</a>";
-    echo "<p>".$row['Description']."</p>";
-    echo "</div>";
-    echo "</div>";
-  }
-}
-?>
-</div>
+    <?php
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+        echo "<div class='Partijvak'>";
+        echo "<img src='" . $row['IMG'] . "'class='Partijlogo'>";
+        echo "<div class='Partijoprij'>";
+        echo "<a href='persoonselectie.php?Partij=" . $row['ID'] . "' class='Partijknop'>" . $row['Name'] . "</a>";
+        echo "<p>" . $row['Description'] . "</p>";
+        echo "</div>";
+        echo "</div>";
+      }
+    }
+    ?>
+  </div>
 </body>
-</html>
 
+</html>
--- persoonselectie.php
+++ persoonselectie.php
@@ -2,115 +2,32 @@
 include("assets/includes/conf.php");
 include("assets/includes/header.php");
 
-$sql = "SELECT ID, FirstName, LastName, IMG, PartyID FROM `members`;";
+
+$sql = "SELECT ID, FirstName, LastName, IMG, PartyID FROM `members` WHERE PartyID = " . $_GET['Partij'] . ";";
 $result = $con->query($sql);
 ?>
 
 <body>
-<form method="post" action="">
-<?php
-if($result->num_rows > 0){
-  while($row = $result->fetch_assoc()){
-    echo "<input type='radio' value=".$row['ID']." id=".$row['ID']." name='member'>";
-    echo  "<label for=".$row['ID'].">".$row['FirstName']." ".$row['LastName']."</label>";
-  }
-}
-?>
-</form>
+  <form method="post" action="">
+    <div class="container">
+      <?php
+      if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+          echo "<div class='Partijvak'>";
+          echo "<img src='" . $row['IMG'] . "'class='Partijlogo'>";
+          echo "<input type='radio' value=" . $row['ID'] . " id=" . $row['ID'] . " name='member' class='persoonselect'>";
+          echo  "<label for=" . $row['ID'] . ">" . $row['FirstName'] . " " . $row['LastName'] . "</label>";
+          echo "</div>";
+        }
+      }
+      ?>
+      <button type="submit" class="verstuurknop">Verstuur</button>
+    </div>
 
 
-       <!-- voorbeeld dropdown -->
-    <!-- <form method="post" action="index.php">
-        <input type="radio" value="klaas" id="1" name="persoon">
-        <label for="1">Klaas</label>
-        <input type="radio" value="Kees" id="2" name="persoon">
-        <label for="2">Kees</label>
-        <input type="radio" value="Karel" id="3" name="persoon">
-        <label for="3">Karel</label>
-        <input type="radio" value="Koos" id="4" name="persoon">
-        <label for="4">Koos</label>
-        <input type="submit" value="submit">
-    </form> -->
+  </form>
+  <a href="partijselectie.php" class="terugknop">Terug</a>
 
-     <!-- dropdown VVD -->
-    <!-- <div class="w3-dropdown-click">
-  <button onclick="myFunction()" class="w3-button w3-black">VVD</button>
-  <div id="Demo" class="w3-dropdown-content w3-bar-block w3-border">
-  <form method="post" action="index.php">
-        <input type="radio" value="klaas" id="1" name="persoon">
-        <label for="1">Klaas</label>
-        <input type="radio" value="Kees" id="2" name="persoon">
-        <label for="2">Kees</label>
-        <input type="radio" value="Karel" id="3" name="persoon">
-        <label for="3">Karel</label>
-        <input type="radio" value="Koos" id="4" name="persoon">
-        <label for="4">Koos</label>
-        <input type="submit" value="submit">
-    </form>
-  </div>
-</div>
-
-© dropdown PVDA -->
-<!-- <div class="w3-dropdown-click">
-  <button onclick="myFunction2()" class="w3-button w3-black">PVDA</button>
-  <div id="Demo2" class="w3-dropdown-content w3-bar-block w3-border">
-  <form method="post" action="index.php">
-        <input type="radio" value="jan" id="1" name="persoon">
-        <label for="1">jan</label>
-        <input type="radio" value="jaap" id="2" name="persoon">
-        <label for="2">jaap</label>
-        <input type="radio" value="joost" id="3" name="persoon">
-        <label for="3">joost</label>
-        <input type="radio" value="jeroen" id="4" name="persoon">
-        <label for="4">jeroen</label>
-        <input type="submit" value="submit">
-    </form>
-  </div>
-</div> -->
-
-
-$sql = "SELECT ID, FirstName, LastName, IMG, PartyID FROM `members` WHERE PartyID = ".$_GET['Partij'].";";
-$result = $con->query($sql);
-?>
-
-<body>
-<form method="post" action="">
-<div class="container">
-<?php
-if($result->num_rows > 0){
-  while($row = $result->fetch_assoc()){
-    echo "<div class='Partijvak'>";
-    echo "<img src='".$row['IMG']."'class='Partijlogo'>";
-    echo "<input type='radio' value=".$row['ID']." id=".$row['ID']." name='member' class='persoonselect'>";
-    echo  "<label for=".$row['ID'].">".$row['FirstName']." ".$row['LastName']."</label>";
-  echo "</div>";
-
-<!-- <script>
-function myFunction() {
-  var x = document.getElementById("Demo");
-  if (x.className.indexOf("w3-show") == -1) { 
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-
-  }
-}
-?>
-<button type="submit" class="verstuurknop">Verstuur</button>
-</div>
-
-
-</form>
-<a href="partijselectie.php" class="terugknop">Terug</a>
-=======
-function myFunction2() {
-  var x = document.getElementById("Demo2");
-  if (x.className.indexOf("w3-show") == -1) { 
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
-</script> --> 
 </body>
+
 </html>
