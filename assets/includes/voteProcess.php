d7a204a8f1acfddb7abaac514919dd6766336aa6
Jocely <jocelyjoy@outlook.com>
2022-03-07 23:09:30 +0100

    --

--- assets/includes/voteProcess.php
+++ assets/includes/voteProcess.php
@@ -10,7 +10,7 @@ $sql = "UPDATE party SET Votes = Votes+1 WHERE ID =". $_POST['Partij'] ;
 if($conn->query($sql)){
     $sql = "UPDATE `codes` SET `used` = 1 WHERE `code` = ". (int)$_SESSION['code']." ";
     if($conn->query($sql)){
-//header('location:../../bedankt.php');
+        header('location:../../bedankt.php');
     }else{
     var_dump((int)$_SESSION['code']);
     }
