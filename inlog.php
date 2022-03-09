ebd815f906cba6dc040597e4dafae5fda78bc8b0
Robin <robinmolenaar662@gmail.com>
2022-03-09 15:16:23 +0100

    kleine dingetjes mooier gemaakt

--- inlog.php
+++ inlog.php
@@ -11,12 +11,12 @@ $errorMessage = $_GET['error'];
         <div class="heading-text-container">
         <form action="assets/includes/process.php" method="POST">
             <h1>Vul hier uw code</h1>
-            <p>
+            <p class='code-container'>
                 <label>Uw code:</label>
                 <input class="code-input" type="password" id="code" name="code" />
             </p>
             <p>
-                <input class="welcome-button" type="submit" id="inlog-btn" name="submit" value="Login" />
+                <input class="button-32" type="submit" id="inlog-btn" name="submit" value="Login" />
             </p>
             <p>
                 <?php if(isset($errorMessage)){echo $errorMessage;} ?>
